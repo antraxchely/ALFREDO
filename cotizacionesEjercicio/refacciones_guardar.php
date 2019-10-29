03b42d6ad7c3dde5b312b18f4bbc97b12dc767b1
antraxchely <54405828+antraxchely@users.noreply.github.com>
2019-10-29 08:15:05 -0600

    Add files via upload

--- cotizacionesEjercicio/refacciones_guardar.php
+++ cotizacionesEjercicio/refacciones_guardar.php
@@ -6,7 +6,8 @@ $nombre_refaccion_post = strtoupper($_POST['nombre_de_refaccion']);
 $descripcion_refaccion_post = strtoupper($_POST['descripcion_de_refaccion']);
 $refaccion_imagen="sin imagen";
 
-$sel = $con->prepare("SELECT refaccion_id,marca_id,refaccion_nombre FROM refaccion where marca_id=? AND refaccion_nombre=?");
+
+/*$sel = $con->prepare("SELECT refaccion_id,marca_id,refaccion_nombre FROM refaccion where marca_id=? AND refaccion_nombre=?");
 $sel->bind_param('is', $marca_id_post, $nombre_refaccion_post);
 $sel->execute();
 $res = $sel->get_result();
@@ -17,7 +18,8 @@ if ($row != 0) {
     echo "¿Deseas agregarle un nuevo precio de proveedor?";
     echo "<a href=\"refacciones_proveedores.php?refaccion_id=" . $refaccion_id . "&refaccion_nombre=" . $nombre_refaccion_post . "\" class=\"btn btn-primary\" role=\"button\"> AGREGAR </a>";
     echo "<a href=\"index_refacciones.php\" class=\"btn btn-default\" role=\"button\"> CANCELAR </a>";
-} else {
+} else {-->*/
+
     $ins = $con->prepare("INSERT INTO refaccion VALUES(?,?,?,?,?)");
     $ins->bind_param("iisss", $id, $marca_id_post, $nombre_refaccion_post, $descripcion_refaccion_post, $refaccion_imagen);
     if ($ins->execute()) {
@@ -28,5 +30,5 @@ if ($row != 0) {
     }
     $ins->close();
     $con->close();
-}
+
 ?>
